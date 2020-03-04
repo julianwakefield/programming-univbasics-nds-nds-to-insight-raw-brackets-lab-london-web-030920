81958227e331cdb2b2b54c3d616228a792ace76b
Julian Wakefield <julianwakefield@outlook.com>
2020-03-04 11:25:49 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,11 +3,11 @@ require 'directors_database'
 
 def directors_totals(nds)
 
-print
 
-  nil
+
+
 end
-
+nil
 
 # Remember, it's always OK to pretty print what you get *in* to make sure
 # that you know what you're starting with!
